09d7d648e257d97ba55473568fdd88e5ffb6d724
Filippo Santovito <filippo.santovito@telematicoaccise.it>
2016-02-24 09:17:03 +0100

    preparing repository for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'miniluv',
+  packages = ['miniluv'],
+  version = '0.1',
+  description = 'a viewmodel library for PySide',
+  author = 'Filippo Santovito',
+  author_email = 'filippo.santovito@gmail.com',
+  url = 'https://github.com/fsantovito/miniluv',
+  download_url = 'https://github.com/fsantovito/miniluv/tarball/0.1',
+  keywords = ['MVVM', 'viewmodel', 'pyside', 'databinding'],
+  classifiers = [],
+)
